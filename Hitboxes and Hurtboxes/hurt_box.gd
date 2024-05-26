19a6b455bfd42dad5c4d48bfbc64d4ad7c2777ce
Aiden <aidenlh@qq.com>
2024-05-26 23:51:23 +0800

    fix: 修复草上也有打击动画的问题

--- Hitboxes and Hurtboxes/hurt_box.gd
+++ Hitboxes and Hurtboxes/hurt_box.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
+@export var show_hit:bool = true
+
 # 预加载打击特效
 const HitEffect = preload("res://Effects/hit_effect.tscn")
 
 func _on_area_entered(area):
-	var effect = HitEffect.instantiate()
-	var main = get_tree().current_scene
-	main.add_child(effect)
-	effect.global_position = global_position - Vector2(0, 8)
+	if show_hit:
+		var effect = HitEffect.instantiate()
+		var main = get_tree().current_scene
+		main.add_child(effect)
+		effect.global_position = global_position - Vector2(0, 8)
