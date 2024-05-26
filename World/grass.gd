be8c4c99a581430ce1614e149087ea7b44b2d2eb
Aiden <aidenlh@qq.com>
2024-05-26 13:31:37 +0800

    feat: p11 新增hurtbox和hitbox，并让hurtbox随角色方向旋转

--- World/grass.gd
+++ World/grass.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		# 实例化特效场景为一个节点
-		var grassEffect = load("res://Effects/grass_effect.tscn")
-		var grassEffectInstance = grassEffect.instantiate()
-		# 获取到本场景的场景树，并为本草在场景中添加一个特效
-		var world_scene_tree = get_tree().current_scene
-		world_scene_tree.add_child(grassEffectInstance)
-		# 将特效的位置放在本草的位置上
-		grassEffectInstance.global_position = global_position
-		
-		# 当帧结束时释放资源，非立即释放，立即释放的函数 free()
-		queue_free()
+func create_grass_effect():
+	# 实例化特效场景为一个节点
+	var grassEffect = load("res://Effects/grass_effect.tscn")
+	var grassEffectInstance = grassEffect.instantiate()
+	# 获取到本场景的场景树，并为本草在场景中添加一个特效
+	var world_scene_tree = get_tree().current_scene
+	world_scene_tree.add_child(grassEffectInstance)
+	# 将特效的位置放在本草的位置上
+	grassEffectInstance.global_position = global_position	
+
+func _on_hurt_box_area_entered(area):
+	create_grass_effect()
+	# 当帧结束时释放资源，非立即释放，立即释放的函数 free()
+	queue_free()
