6e65e762480dfc5057d879fe0e2df723b3da759c
Aiden <aidenlh@qq.com>
2024-05-26 16:46:09 +0800

    feat: p12 新增翻滚动作

--- Player/Player.gd
+++ Player/Player.gd
@@ -12,6 +12,9 @@ const ACCELERATION = 500.0
 # 状态机变量
 enum {MOVE, ROLL, ATTACK}  # enum自动将其中的量设置为 0, 1, 2 ...
 var state = MOVE
+# 翻滚所需变量
+var roll_vector = Vector2.DOWN
+const ROLL_SPEED = 125
 
 # 初始化调用
 func _ready():
@@ -34,10 +37,13 @@ func move_state(delta):
 	input_vector.y = Input.get_axis("ui_up", "ui_down")
 	
 	if input_vector != Vector2.ZERO:
+		# 确定翻滚方向
+		roll_vector = input_vector
 		# 根据动画树确定方向
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		# 动画切换到Run
 		animationState.travel("Run")
 		# 将向量标准化，限制在一个圆中而不是方形，速度逐渐提升到MAX_SPEED
@@ -52,15 +58,24 @@ func move_state(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, ACCELERATION * delta)
 	move_and_slide()
 	
+	# 检测按键事件
+	if Input.is_action_pressed("roll"):
+		state = ROLL
 	if Input.is_action_pressed("attack"):
 		state = ATTACK
 
 func roll_state(_delta):
-	pass
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("Roll")
+	move_and_slide()
 	
 func attack_state(_delta):
 	velocity = Vector2.ZERO
 	animationState.travel("Attack")
+
+# 翻滚动画播放完成后调用的方法
+func roll_animation_finished():
+	state = MOVE
 
 # 攻击动画播放完成后调用的方法
 func attack_animation_finished():
