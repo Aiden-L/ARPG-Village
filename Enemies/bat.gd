7e4dbf9465ae49a39b2119680786a823aa2328eb
Aiden <aidenlh@qq.com>
2024-05-28 17:33:07 +0800

    feat: p21 新增敌人空闲时移动AI

--- Enemies/bat.gd
+++ Enemies/bat.gd
@@ -7,15 +7,22 @@ const EnemyDeathEffect = preload("res://Effects/enemy_death_effect.tscn")
 @onready var animated_sprite = $AnimatedSprite
 # 设置软碰撞
 @onready var soft_collision = $SoftCollision
+# wander controller
+@onready var wander_controller = $WanderController
 
 @export var ACCELERATION = 300
 @export var MAX_SPEED = 50
 @export var FRICTION = 200
 
+# 由于目标无法精确到达指定点，因此会在目标点附近徘徊，设置此值，当距离处于此值内时，判定已经到达目标点
+@export var WANDER_TARGET_RANGE = 4
+
 # 状态机
 enum {IDLE, WANDER, CHASE}
 
 var state = IDLE
+func _ready():
+	state = pick_random_state([IDLE, WANDER])
 
 func _physics_process(delta):
 	# 设置击退效果，（摩擦力）
@@ -24,24 +31,50 @@ func _physics_process(delta):
 	match state:
 		IDLE: 
 			seek_player()
-		WANDER: pass
+			if wander_controller.get_time_left() == 0:
+				update_wander()
+		WANDER:
+			seek_player()
+			if wander_controller.get_time_left() == 0:
+				update_wander()
+			var direction = global_position.direction_to(wander_controller.target_position)
+			# 朝向启动方向，移动
+			animated_sprite.flip_h = velocity.x < 0
+			velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			
+			# 到新地点之后重新选择状态，避免来回移动
+			if global_position.distance_to(wander_controller.target_position) <= WANDER_TARGET_RANGE:
+				update_wander()
 		CHASE: 
 			# 朝向玩家
 			animated_sprite.flip_h = velocity.x < 0
 			# 追逐玩家
 			var player = player_detection_zone.player
 			if player != null:
-				var direction = (player.global_position - global_position).normalized()
+				var direction = global_position.direction_to(player.global_position)
 				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
 	# 设置软碰撞
 	if soft_collision.is_colliding():
 		velocity += soft_collision.get_push_vector() * delta * 400
 	move_and_slide()
 
+# 此封装方法暂不使用
+#func accelerate_towards_point(point, delta):
+	#var direction = global_position.direction_to(point)
+	#velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+	#animated_sprite.flip_h = velocity.x < 0
+
+func update_wander():
+	state = pick_random_state([IDLE, WANDER])
+	wander_controller.start_wander_timer(randf_range(1, 3))
+
 # 发现player追逐player
 func seek_player():
 	if player_detection_zone.can_see_player():
 		state = CHASE
+
+func pick_random_state(state_list):
+	return state_list.pick_random()
 
 func _on_hurt_box_area_entered(area):
 	# 获取进入区域中的变量（在剑脚本中设定了朝向）
--- README.md
+++ README.md
@@ -2,3 +2,36 @@
 This is a ARPG game demo called Village. Using Godot Engine.
 
 Godot Version = 4.2
+
+### 项目说明
+
+### 提交记录
+* feat: p20 添加摄像机随角色移动，fix: 修复血量UI不随摄像机移动的问题，修复角色死亡后摄像机归位的问题
+* feat: p19 给敌人增加软碰撞
+* feat: p18 引入树，给玩家/bush添加影子
+* feat: p18 新增生命值UI
+* fix: 更改hurtbox的实现方式，增加攻击之后的延迟，解决collision重叠时无法触发攻击的问题
+* feat: p17 角色属性创建为全局自动加载单例，新增敌人攻击角色（hitbox，hurtbox）
+* feat: p16 新增玩家检测区域，新增敌人状态机，新增敌人追逐AI
+* fix: 修复草上也有打击动画的问题
+* feat: p16 新增打击特效
+* fix: 添加特效采用用其父节点生成子节点的方法，修复草特效出现在角色上层的问题，添加敌人死亡动画
+* fix: 将load改为preload
+* feat: p15 将base_effect.tscn抽离成一个抽象的scene（称为模板），若要使用模板，其脚本采用继承的方式，继承effect.gd
+* fix: 修复游戏开始时动作方向与朝向不同的问题，将AniTree中不同动作的初始指向统一（这里设置为向下）
+* feat: p14 新增敌人属性（血量），创建角色伤害继承关系
+* feat: p13 新建敌人bat，设置hurtbox，添加受打击后的击退效果
+* feat: p12 新增翻滚动作
+* feat: p11 更改碰撞检测层，使Hurtbox只处于Hitbox能够碰撞的层，同时更改场景的碰撞层级关系
+* feat: p11 新增hurtbox和hitbox，并让hurtbox随角色方向旋转
+* feat: p10 新增草消失动画，创建动画结束信号关联，实时实例化特效节点
+* feat: p9 新增攻击动画，新增简单的状态机
+* feat: p8 新增有碰撞体的TileMap
+* feat: p7 TileMap设置，背景设置，路径地形绘制
+* feat: p6 建立动画树，关联方向动画
+* feat: p5 新建动画
+* feat: p4 y sort 确定层级
+* feat: p3 创建角色碰撞体，场景碰撞面积
+
+### 问题记录
+- p20: Godot 3 中似乎存在每次运行游戏时，随机数种子不变的问题，解决方法是在任意位置调用`randomize()`方法， 但 Godot 4 中似乎不存在这个问题
